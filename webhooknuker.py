ed2cd384a6026c8425296a350b711a15e504454b
Din <73368499+20cmDuro@users.noreply.github.com>
2022-03-07 18:42:13 +0000

    Update webhooknuker.py

--- webhooknuker.py
+++ webhooknuker.py
@@ -1,23 +1,8 @@
-import requests, os, time
+import requests, time, utils
 from colorama import Fore, Back, Style
-import os as sistema
-from os import system
 from discord import Webhook, RequestsWebhookAdapter
 
-user = sistema.getlogin()
-os.system("cls")
-
-print(Fore.MAGENTA + Style.DIM + Style.BRIGHT + """
-     __    __     _     _                 _        ___ _               _    
-    / / /\ \ \___| |__ | |__   ___   ___ | | __   / __\ |__   ___  ___| | __
-    \ \/  \/ / _ \ '_ \| '_ \ / _ \ / _ \| |/ /  / /  | '_ \ / _ \/ __| |/ /
-     \  /\  /  __/ |_) | | | | (_) | (_) |   <  / /___| | | |  __/ (__|   < 
-      \/  \/ \___|_.__/|_| |_|\___/ \___/|_|\_\ \____/|_| |_|\___|\___|_|\_\
-                                                                        
-""" + Style.RESET_ALL)
-
-system(f"title 𝑪𝒓𝒆𝒂𝒕𝒐𝒓: 𝐃𝐞𝐛𝐨𝐜𝐡𝐞𝐝#𝟒𝟒𝟎𝟒 - 𝑫𝑩𝑶𝑿 𝓦𝓮𝓫𝓱𝓸𝓸𝓴 𝐂𝐡𝐞𝐜𝐤 - 𝐔𝐬𝐞𝐫: [{user}]")
-
+utils.checker_intro()
 webhook = input(Fore.MAGENTA + Style.DIM + " [DB0X] URL: ")
 print(Style.RESET_ALL)
 
@@ -27,8 +12,9 @@ if check.status_code == 404:
     time.sleep(5)
 elif check.status_code == 200:
     print(Fore.GREEN + Style.BRIGHT + "\n [DB0X] Webhook valida!" + Style.RESET_ALL)
-    print(Fore.YELLOW + Style.BRIGHT + "\n [DB0X] Deletando a Webhook...!" + Style.RESET_ALL)
     time.sleep(1)
+    utils.killer_intro()
+    print(Fore.YELLOW + Style.BRIGHT + "\n [DB0X] Deletando a Webhook...!" + Style.RESET_ALL)
     requests.delete(webhook)
     checker = requests.get(webhook)
     if checker.status_code == 404:
